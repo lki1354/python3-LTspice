5109d55c08633143f768c58e8f29aed3837224cc
Lukas Kiechle <lki1354@students.fhv.at>
2017-11-08 07:07:39 +0100

    add example with Buck Converter
    the SimulationBuck.py has to be moved to the top dir and also the *.cmd file

---
+++ SimulationBuck.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+from LTspice.Circuits import Converter
+simulation_dir = 'E:\Qsync\git\Examples'
+
+
+L1 = 33E-6
+L2 = 33E-6
+CC = 4.7E-6
+Lin = 33E-6
+Cin = 33E-6
+Cout = 4.7E-6
+Uin = 48
+
+f = 1.0E+6
+T = 1/f
+Uout = 24
+Iout = 5
+#Rload = 0.001
+Rload = Uout / Iout
+Rload
+
+Buck = Converter(simulation_dir,'BuckVariable.asc',{'L1':L1,'Cout':Cout,'Uin':Uin,'Rload':Rload,'Tperiode':T,'Ton':(T*Uout/Uin)})
+Buck.run()
+print(Buck)
+
+
